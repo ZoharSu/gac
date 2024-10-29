c49ddc43f48168ccb36bf2f212ec7bca070a55f0
zoxarc <zoxarc@gmail.com>
2024-10-29 22:20:19 +0200

    Define allocation pointer type

--- src/gac.h
+++ src/gac.h
@@ -5,5 +5,12 @@
 
 #ifdef GAC_PRIVATE_HEADER
 
+typedef gacptr uintptr_t;
+
+typedef struct gac_alloc_t {
+    gacptr ptr;
+    size_t len;
+} gac_alloc_t;
+
 #endif // GAC_PRIVATE_HEADER
 #endif // GAC_HEADER_INCLUDED
