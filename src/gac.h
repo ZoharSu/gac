0af6945b9b239ced451c31112c314fb475ba116f
zoxarc <zoxarc@gmail.com>
2024-10-29 22:22:36 +0200

    Declare Linked List functions

--- src/gac.h
+++ src/gac.h
@@ -12,5 +12,27 @@ typedef struct gac_alloc_t {
     size_t len;
 } gac_alloc_t;
 
+/* --------------- Linked List ----------------- */
+
+typedef struct LinkedList {
+    gac_alloc_t elem;
+
+    struct LL *next;
+} LL;
+
+typedef struct {
+    LL *head;
+} LL_head;
+
+size_t ll_length(const LL_head h);
+
+void ll_push_back(LL_head *h, LL_T elem);
+
+void ll_destroy(LL_head *h);
+
+void ll_remove(LL_head *h, size_t i);
+
+LL *ll_get(LL_head *h, size_t i);
+
 #endif // GAC_PRIVATE_HEADER
 #endif // GAC_HEADER_INCLUDED
